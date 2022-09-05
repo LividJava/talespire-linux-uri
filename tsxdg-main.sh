1a7213c9eca0d70cecbd3e0a94266c24c3ebf9ca
LividJava <lividjava@gmail.com>
2022-09-05 07:55:13 -0400

    Inital creation of files

---
+++ tsxdg-main.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+WHEREAMI=$(pwd)
+
+echo "Welcome to the UNOFFICIAL Talespire URI Install script!"
+echo "Please note, this is a Community Script, and is NOT OFFICIALLY SUPPORTED!"
+echo ""
+echo "This script assumes that you are using DEFAULT install paths for Steam AND Talespire."
+echo "If you are not, you MUST install manually!"
+echo ""
+
+sleep 5
+
+clear
+echo "Testing to see if you have something configured already"
+
+#Look for the common typo files if they exist at all.
+if
+    [ -f ~/.local/share/applications/talespire-xdg.desktop ] || [ -f ~/.local/share/applications/xdg-talespire.desktop ]
+then
+    echo "We can see a file! We'll try to use the existing file!"
+    #it's jank but assign the existing file to a variable for use later.
+    if
+        [ -f ~/.local/share/applications/talespire-xdg.desktop ]
+    then
+        JANKFILE=~/.local/share/applications/talespire-xdg.desktop
+    else
+        JANKFILE=~/.local/share/applications/xdg-talespire.desktop
+    fi
+    #set a variable equal to whatever xdg-mime has
+    TSXDG=$(xdg-mime query default x-scheme-handler/talespire)
+    mv $JANKFILE $TSXDG
+    echo ""
+    echo "We've made the existing file match MIME, please test by running 'xdg-open talespire://dice/TEST:1d20'"
+    echo "Talespire should open and roll a single D20 labeled TEST"
+    clear
+    exit 0
+else
+echo "We could not find an existing file! Let's get you set up!"
+wget -N "https://github.com/lividjava/talespire-linux-uri/main/tsxdg-setup.sh" && bash tsxdg-setup.sh
+echo ""
+fi
---
+++ tsxdg-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#We need to test if the config file is a default Steam Proton or something else.
+
+#Read the version file and save it as a variable
+echo "Getting Proton Version of Talespire."
+TSVERSION=$(cat ~/.steam/steam/steamapps/compatdata/720620/version)
+if [[ $TSVERSION == *"Proton"* ]];
+# Exit early if the version file contains proton. We're done.
+break
+else
+#Add the word Proton to the variable because Valve loves spaces.
+TSVERSION="Proton\ $TSVERSION"
+fi
+
+echo "Creating the MIME file."
+echo "Thanks to Doskious and Adriannom on Discord!"
+#Create the base configuration file, We'll add the Exec path later
+echo "[Desktop Entry]
+Type=Application
+Name=TaleSpire
+StartupNotify=false
+MimeType=x-scheme-handler/talespire;" | tee ~/.local/applications/xdg-talespire.desktop;
+
+
+echo "Exec=/bin/bash -c \'env WINEPREFIX=\"/home/$USER/.steam/steam/steamapps/compatdata/720620/pfx\" WINEESYNC=1 /home/$USER/.steam/steam/steamapps/common/$TSVERSION/dist/bin/wine64 /home/$USER/.steam/steam/steamapps/common/TaleSpire/TaleSpireUrlRelay.exe %u\'" >> ~/.local/applications/xdg-talespire.desktop
+
+echo "Adding the MIME type."
+xdg-mime default xdg-talespire.desktop x-scheme-handler/talespire
+
+echo "And we're done! Test if everything is setup correctly with 'xdg-open talespire://dice/TEST:1d20'"
+echo "This will roll a d20 labeled TEST in Talespire if everything is setup properly"
+#Clean up after ourselves!
+rm "tsxdg-setup.sh"
+exit 0
